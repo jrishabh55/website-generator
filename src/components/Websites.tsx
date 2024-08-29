ce8af3296a263fa00a30e0cc4fab11af346628ef
Rishabh Jain <hello@rishabhjain.dev>
2024-08-29 21:06:45 +0530

    Refactor Websites component to handle null websites object

--- src/components/Websites.tsx
+++ src/components/Websites.tsx
@@ -5,6 +5,11 @@ import { Card } from "./ui/card";
 
 const Websites: FC<{ username: string }> = async ({ username }) => {
   const websites = await getWebsites(username);
+
+  if (!websites) {
+    return null;
+  }
+
   return (
     <Card className="flex flex-col gap-y-4 p-4 w-60">
       <h1>Existing Websites</h1>
